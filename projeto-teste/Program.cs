d3145fee5e52e2f42d4d60cb521c7cc99ad3491b
Marivaldo Lacerda <mmarivaldo@hotmail.com>
2020-06-11 16:53:46 -0300

    Acrescentada a mensagem Bom dia!

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -7,6 +7,7 @@ namespace projeto_teste
         static void Main(string[] args)
         {
             Console.WriteLine("Ola Mundo!");
+            Console.WriteLine("Bom dia!");
         }
     }
 }
